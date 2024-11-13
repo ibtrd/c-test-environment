0e2973735c86466cfc58c76dd130cf5de9b9179e
ibertran <128724498+ibtrd@users.noreply.github.com>
2024-11-13 11:32:25 +0100

    feat: add sizeof() printing program
    Co-authored-by:  lohhiiccc <96543753+lohhiiccc@users.noreply.github.com>

---
+++ tests/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("sizeof(int)   : %d\n", sizeof(int));
+    printf("sizeof(long)  : %d\n", sizeof(long));
+    printf("sizeof(void *): %d\n", sizeof(void *));
+    printf("sizeof(short) : %d\n", sizeof(short));
+}
